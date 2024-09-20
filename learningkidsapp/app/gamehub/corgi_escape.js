840828a74f8a8d55d02f9840fddd82791313b653
alexismatias <matias.j.alexis@gmail.com>
2024-09-20 05:10:27 -0500

    Create corgi escape front end

--- learningkidsapp/app/gamehub/corgi_escape.js
+++ learningkidsapp/app/gamehub/corgi_escape.js
@@ -1,9 +1,12 @@
 
-import { Alert, Button, Image, Pressable, SafeAreaView, StyleSheet, Switch, Text, TextInput, View } from 'react-native';
+import { Alert, Button, Image, ImageBackground, Modal, Pressable, SafeAreaView, StyleSheet, Switch, Text, TextInput, View } from 'react-native';
 import React, { useState } from 'react';
+
 export default function Page() {
   return (
+    <ImageBackground source={require('../../assets/CorgiEscapeBG.png')} style={styles.background}>
     <SafeAreaView style={styles.container}>
+
       <View style={styles.hearts}>
         <Image source={require('../../assets/heartIcon.png')}style= {{alignItems: 'flex-end'}}/>
         <Image source={require('../../assets/heartIcon.png')}style= {{alignItems: 'flex-end'}}/>
@@ -26,49 +29,59 @@ export default function Page() {
 
 
     </SafeAreaView>
-
+    </ImageBackground>
 
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // Ensures the background image covers the entire screen
+  },
   container: {
     flex: 1,
     //justifyContent: 'center',
     //alignItems: 'center',
     padding: 16, // Padding around the container
-    backgroundColor: '#A7C7E7',
+    background: '#A7C7E7',
     
   },
 
   hearts: {
     flexDirection: 'row',
-    justifyContent: 'right',
+    justifyContent: 'flex-end',
     gap: 5,
   },
 
   score: {
     flexDirection: 'row',
-    justifyContent: 'right',
+    justifyContent: 'flex-end',
   },
 
   scoreText:{
     fontSize: 25,
-
-
+    fontFamily: 'EBGaramond_800ExtraBold',
+    color: 'white',
+    textShadowColor: 'black', // Outline color
+    textShadowOffset: { width: 2, height: 2 }, 
+    textShadowRadius: 1, // Blur radius for the shadow
   },
 
   questions:{
-    flexDirection: 'row',
+    backgroundColor: 'rgba(211, 211, 211, 0.5)',
+    
     borderColor: 'black',
     borderWidth: 1,
     alignSelf: 'baseline',
     alignContent: 'center',
     borderRadius: 15,
-    height: 250,
-    width: 450,
+    height: 200,
+    width: 400,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    textAlign: 'center',
+    fontFamily: 'EBGaramond_800ExtraBold',
   },
 
   questionArea:{
@@ -81,26 +94,28 @@ const styles = StyleSheet.create({
 
   answerArea: {
     //display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
+    alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'space-between',
     justifyContent: 'center',
     paddingHorizontal: 20,
     paddingTop: 50,
-    gap: 75,
-    
+    gap: 20,
+    fontFamily: 'EBGaramond_800ExtraBold',
   },
 
   answers: {
+    backgroundColor: 'rgba(211, 211, 211, 0.5)',
     flexDirection: 'row',
     borderColor: 'black',
     borderWidth: 1,
     alignContent: 'center',
     borderRadius: 15,
-    height: 250,
-    width: 450,
+    height: 100,
+    width: 400,
     justifyContent: 'center',
     paddingHorizontal: 20,
-
+    fontFamily: 'EBGaramond_800ExtraBold',
   },
 });
