86abbdb4b5fc3cbea2918e40d7fe3c9c8ecebac7
IJ <chukwumaijeomachukwuma29@gmail.com>
2024-11-11 18:14:16 -0600

    pushing final code and stuff - IJ

---
+++ learningkidsapp/app/config/questions.js
@@ -0,0 +1,10 @@
+import { ref, onValue } from 'firebase/database';
+import { auth, database } from '../config/firebaseConfig'; // Ensure you import your Firebase configuration
+
+// questions.js
+export const Questions = [
+    { question: "What is {name}'s relationship to you?", key: "relationship", incorrect: 'Wrong' },
+    { question: "What is {name}'s phone number?", key: "phone", incorrect: 'Wrong' },
+    { question: "What is {name}'s address?", key: "address", incorrect: 'Wrong' },
+    //{ question: "What is {relationship} first name?", key: "name", incorrect: 'Wrong'}
+  ];
